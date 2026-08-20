cb79051ed1bc8a2fd1be2b2abbf33a6bf506ca9f
DrGo <salah.mahmud@gmail.com>
2026-08-20 09:32:51 +0800

    use grok light

--- lua/light.lua
+++ lua/light.lua
@@ -1,45 +1,51 @@
--- light.lua: Minimal, eye-friendly colorscheme module for use with grandscheme
--- Returns a table of highlight groups compatible with the loader
+-- light.lua: Grok Day colorscheme for grandscheme
+-- Palette and token mapping from GrokDay + grok-day.tmTheme
+-- (xai-org/grok-build). Returns highlight groups for the loader.
 
--- Base Palette (v2 - Refined for better contrast and comfort)
+-- Grok Day palette
 local colors = {
-	-- Core colors
-	bg                  = '#F5F5F5',
-	fg                  = '#383A42',
-	comment             = '#8E8E96',
-	keyword             = '#B35959',
-	error_red           = '#D95C5C',
-	orange              = '#C17801',
-	type_yellow         = '#9A8100',
-	green               = '#50A14F',
-	cyan                = '#0969DA',
-	blue                = '#4078F2',
-	purple              = '#A626A4',
-	gray                = '#D0D0D0',
+	-- Surfaces
+	bg             = '#EEEEEE',
+	fg             = '#262626',
+	fg_alt         = '#444444',
+	comment        = '#767676',
+	comment_doc    = '#808080',
+	gutter         = '#B2B2B2',
+	gray           = '#D0D0D0',
+	dark3          = '#8E8E8E',
+	dark5          = '#626262',
 
-	-- Enhanced colors for better readability
-	comment_alt         = '#7E7E86', -- Darker comments for better small-text contrast
-	punctuation         = '#A0A0A0', -- Neutral gray for punctuation/delimiters
-	diag_error_alt      = '#C94C4C', -- Softer error undercurl on light bg
-	diag_error_lighter  = '#FF3333', -- Softer error on black bg
+	-- Accents (deepened TokyoNight family)
+	blue           = '#2F64D2',
+	blue0          = '#28448A',
+	blue1          = '#0F87A2',
+	cyan           = '#0082AA',
+	green          = '#378E23',
+	green1         = '#0C947C',
+	magenta        = '#7D4BC6',
+	purple         = '#6C3EB2',
+	orange         = '#C3691E',
+	yellow         = '#A27612',
+	teal           = '#0A8E70',
+	red            = '#CD3048',
+	red1           = '#AF2323',
 
-	-- Tree-sitter specific overrides
-	ts_constant         = '#9A8100', -- @constant
-	ts_constant_builtin = '#B35959', -- @constant.builtin
+	-- Operators / punctuation (tmTheme)
+	operator       = '#5580A8',
+	punctuation    = '#4A72B0',
 
-	-- VCS diff background colors
-	diff_add_bg         = '#D6EED1', -- DiffAdd
-	diff_change_bg      = '#FFF7C2', -- DiffChange
-	diff_delete_bg      = '#F8D6D9', -- DiffDelete
+	-- UI surfaces
+	bg_cursor      = '#DEDEDE',
+	bg_visual      = '#C6C6C6',
+	bg_pmenu       = '#F0F0F0',
+	bg_float       = '#F0F0F0',
+	border_float   = '#DEDEDE',
+	bg_search      = '#A27612',
 
-	-- UI background variants
-	bg_pmenu            = '#F8F8F8', -- Pmenu
-	bg_cursor           = '#E2E4E8', -- CursorLine/CursorColumn
-	bg_float            = '#F0F0F0', -- CHANGE THIS from #FBFBFB
-	border_float        = '#CCCCCC', -- FloatBorder
-	-- Plugin specific
-	fzf_lua_help_fg     = '#6C9A00', -- fzf-lua help line override
-
+	-- Diff
+	diff_add_bg    = '#DAF2DC',
+	diff_change_bg = '#F5EED8',
+	diff_delete_bg = '#F5DADE',
 }
 
 -- Highlight table returned for loader
@@ -50,26 +56,33 @@ local scheme = {
 	Normal                     = { fg = colors.fg, bg = colors.bg },
 	CursorLine                 = { bg = colors.bg_cursor },
 	CursorColumn               = { bg = colors.bg_cursor },
-	ColorColumn                = { bg = '#E8E8E8' },
-	LineNr                     = { fg = '#B0B0B0', bg = colors.bg },
-	CursorLineNr               = { fg = colors.orange, bg = colors.bg, bold = true },
+	ColorColumn                = { bg = colors.bg_cursor },
+	LineNr                     = { fg = colors.gutter, bg = colors.bg },
+	CursorLineNr               = { fg = colors.yellow, bg = colors.bg, bold = true },
 	WinSeparator               = { fg = colors.gray },
-	StatusLine                 = { fg = colors.fg, bg = '#E0E0E0' },
-	StatusLineNC               = { fg = '#A0A0A0', bg = '#E0E0E0' },
+	StatusLine                 = { fg = colors.fg_alt, bg = colors.bg_cursor },
+	StatusLineNC               = { fg = colors.comment, bg = colors.bg_cursor },
 	VertSplit                  = { fg = colors.gray },
-	SignColumn                 = { fg = colors.fg, bg = colors.bg },
-	Visual                     = { bg = '#D0E0FF' },
-	MatchParen                 = { fg = colors.error_red, bold = true },
+	SignColumn                 = { fg = colors.gutter, bg = colors.bg },
+	Visual                     = { bg = colors.bg_visual },
+	MatchParen                 = { fg = colors.orange, bold = true },
+	Folded                     = { fg = colors.comment, bg = colors.bg_cursor },
+	NonText                    = { fg = colors.gutter },
+	Whitespace                 = { fg = colors.gutter },
+	EndOfBuffer                = { fg = colors.bg },
+	Substitute                 = { fg = colors.bg, bg = colors.red },
+	TODO                       = { fg = colors.bg, bg = colors.yellow, bold = true },
 
 	-- Popup menus
 	Pmenu                      = { fg = colors.fg, bg = colors.bg_pmenu },
-	PmenuSel                   = { fg = colors.bg, bg = colors.blue },
+	PmenuSel                   = { fg = colors.fg, bg = colors.bg_cursor },
 	NormalFloat                = { fg = colors.fg, bg = colors.bg_float },
 	FloatBorder                = { fg = colors.border_float },
 
 	-- Search
-	Search                     = { fg = colors.fg, bg = '#D7BA7D' },
+	Search                     = { fg = colors.bg, bg = colors.yellow },
 	IncSearch                  = { fg = colors.bg, bg = colors.orange },
+	CurSearch                  = { fg = colors.bg, bg = colors.orange, bold = true },
 
 	-- Diff
 	DiffAdd                    = { bg = colors.diff_add_bg },
@@ -79,134 +92,135 @@ local scheme = {
 	-----------------------------------------------------------------------------
 	-- Legacy Syntax Highlighting
 	-----------------------------------------------------------------------------
-	Comment                    = { fg = colors.comment_alt, italic = true },
-	Constant                   = { fg = colors.purple },
+	Comment                    = { fg = colors.comment, italic = true },
+	Constant                   = { fg = colors.orange },
 	String                     = { fg = colors.green },
-	Identifier                 = { fg = colors.blue },
+	Identifier                 = { fg = colors.magenta },
 	Function                   = { fg = colors.blue },
-	Statement                  = { fg = colors.keyword },
-	Keyword                    = { fg = colors.keyword, bold = true },
-	Operator                   = { fg = colors.fg },
-	Type                       = { fg = colors.type_yellow },
-	Special                    = { fg = colors.cyan },
+	Statement                  = { fg = colors.magenta },
+	Keyword                    = { fg = colors.magenta },
+	Operator                   = { fg = colors.cyan },
+	Type                       = { fg = colors.blue1 },
+	Special                    = { fg = colors.blue },
+	Delimiter                  = { fg = colors.fg_alt },
 
 	-- Diagnostics (LSP)
-	DiagnosticError            = { fg = colors.diag_error_lighter },
-	DiagnosticUnderlineError   = { sp = colors.error_red, undercurl = true },
-	DiagnosticWarn             = { fg = colors.orange },
+	DiagnosticError            = { fg = colors.red },
+	DiagnosticUnderlineError   = { sp = colors.red, undercurl = true },
+	DiagnosticWarn             = { fg = colors.yellow },
 	DiagnosticInfo             = { fg = colors.blue },
-	DiagnosticHint             = { fg = colors.green },
+	DiagnosticHint             = { fg = colors.teal },
 
 	-- LSP References & CodeLens
-	LspReferenceText           = { bg = '#FFEED5' },
-	LspReferenceRead           = { bg = '#FFEED5' },
-	LspReferenceWrite          = { bg = '#FFDCD7' },
-	LspCodeLens                = { fg = '#A0A0A0', italic = true },
+	LspReferenceText           = { bg = colors.bg_cursor },
+	LspReferenceRead           = { bg = colors.bg_cursor },
+	LspReferenceWrite          = { bg = colors.bg_cursor },
+	LspCodeLens                = { fg = colors.comment, italic = true },
 
 	-----------------------------------------------------------------------------
 	-- Spell Checking
 	-----------------------------------------------------------------------------
-	SpellBad                   = { sp = colors.error_red, undercurl = true },
-	SpellCap                   = { sp = colors.orange, undercurl = true },
-	SpellLocal                 = { sp = colors.type_yellow, undercurl = true },
+	SpellBad                   = { sp = colors.red, undercurl = true },
+	SpellCap                   = { sp = colors.yellow, undercurl = true },
+	SpellLocal                 = { sp = colors.blue, undercurl = true },
 	SpellRare                  = { sp = colors.purple, undercurl = true },
 
 	-----------------------------------------------------------------------------
 	-- Git Signs
 	-----------------------------------------------------------------------------
 	GitSignsAdd                = { fg = colors.green },
-	GitSignsChange             = { fg = colors.orange },
-	GitSignsDelete             = { fg = colors.error_red },
+	GitSignsChange             = { fg = colors.yellow },
+	GitSignsDelete             = { fg = colors.red },
 
 	-----------------------------------------------------------------------------
 	-- Tree-sitter Highlights
 	-----------------------------------------------------------------------------
 	-- Comments & Documentation
-	['@comment']               = { fg = colors.comment_alt, italic = true },
-	['@comment.documentation'] = { fg = colors.comment, italic = true },
+	['@comment']               = { fg = colors.comment, italic = true },
+	['@comment.documentation'] = { fg = colors.comment_doc, italic = true },
 
 	-- Constants & Literals
-	['@constant']              = { fg = colors.ts_constant },
-	['@constant.builtin']      = { fg = colors.ts_constant_builtin, bold = true },
-	['@constant.macro']        = { fg = colors.purple },
-	['@boolean']               = { fg = colors.purple },
+	['@constant']              = { fg = colors.orange },
+	['@constant.builtin']      = { fg = colors.orange },
+	['@constant.macro']        = { fg = colors.cyan },
+	['@boolean']               = { fg = colors.orange },
 	['@number']                = { fg = colors.orange },
 	['@float']                 = { fg = colors.orange },
 	['@character']             = { fg = colors.green },
 
 	-- Strings
 	['@string']                = { fg = colors.green },
-	['@string.escape']         = { fg = colors.purple, bold = true },
-	['@string.regex']          = { fg = colors.orange },
+	['@string.escape']         = { fg = colors.magenta },
+	['@string.regex']          = { fg = colors.green1 },
 
 	-- Functions & Methods
 	['@function']              = { fg = colors.blue },
-	['@function.builtin']      = { fg = colors.cyan, bold = true },
+	['@function.builtin']      = { fg = colors.blue },
 	['@function.call']         = { fg = colors.blue },
 	['@function.macro']        = { fg = colors.cyan },
 	['@function.method']       = { fg = colors.blue },
 	['@function.method.call']  = { fg = colors.blue },
-	['@constructor']           = { fg = colors.blue, bold = true },
+	['@constructor']           = { fg = colors.magenta },
 
 	-- Variables & Parameters
 	['@variable']              = { fg = colors.fg },
-	['@variable.builtin']      = { fg = colors.purple, bold = true },
-	['@variable.parameter']    = { fg = colors.fg, italic = true },
+	['@variable.builtin']      = { fg = colors.red },
+	['@variable.parameter']    = { fg = colors.yellow, italic = true },
 
 	-- Types & Keywords
-	['@type']                  = { fg = colors.type_yellow },
-	['@type.builtin']          = { fg = colors.type_yellow, bold = true },
-	['@keyword']               = { fg = colors.keyword, bold = true },
-	['@keyword.function']      = { fg = colors.keyword },
-	['@conditional']           = { fg = colors.keyword, bold = true },
-	['@repeat']                = { fg = colors.keyword, bold = true },
-	['@exception']             = { fg = colors.keyword, bold = true },
-	['@include']               = { fg = colors.keyword },
+	['@type']                  = { fg = colors.blue1 },
+	['@type.builtin']          = { fg = colors.blue1 },
+	['@keyword']               = { fg = colors.magenta },
+	['@keyword.function']      = { fg = colors.magenta },
+	['@conditional']           = { fg = colors.magenta },
+	['@repeat']                = { fg = colors.magenta },
+	['@exception']             = { fg = colors.magenta },
+	['@include']               = { fg = colors.magenta },
 
 	-- Structure & Organization
 	['@namespace']             = { fg = colors.cyan },
-	['@field']                 = { fg = colors.blue },
-	['@property']              = { fg = colors.blue },
-	['@attribute']             = { fg = colors.purple },
+	['@field']                 = { fg = colors.green1 },
+	['@property']              = { fg = colors.green1 },
+	['@attribute']             = { fg = colors.cyan },
 	['@annotation']            = { fg = colors.cyan },
-	['@label']                 = { fg = colors.purple },
+	['@label']                 = { fg = colors.blue },
 
 	-- Punctuation & Operators
-	['@operator']              = { fg = colors.fg },
-	['@punctuation.bracket']   = { fg = colors.fg },
-	['@punctuation.delimiter'] = { fg = colors.punctuation },
-	['@punctuation.special']   = { fg = colors.orange },
+	['@operator']              = { fg = colors.cyan },
+	['@punctuation.bracket']   = { fg = colors.fg_alt },
+	['@punctuation.delimiter'] = { fg = colors.fg_alt },
+	['@punctuation.special']   = { fg = colors.cyan },
 
 	-- Tags & Markup
-	['@tag']                   = { fg = colors.cyan },
-	['@tag.delimiter']         = { fg = colors.comment },
-	['@symbol']                = { fg = colors.purple },
+	['@tag']                   = { fg = colors.red },
+	['@tag.delimiter']         = { fg = colors.dark5 },
+	['@symbol']                = { fg = colors.orange },
 
 	-- Text & Markup (Markdown, etc.)
 	['@text']                  = { fg = colors.fg },
 	['@text.title']            = { fg = colors.blue, bold = true },
 	['@text.strong']           = { bold = true },
 	['@text.emphasis']         = { italic = true },
-	['@text.literal']          = { bg = colors.bg_cursor },
-	['@text.uri']              = { fg = colors.cyan, underline = true },
+	['@text.literal']          = { fg = colors.green },
+	['@text.uri']              = { fg = colors.blue, underline = true },
 	['@markup.heading']        = { fg = colors.blue, bold = true },
 	['@markup.strong']         = { bold = true },
 	['@markup.italic']         = { italic = true },
-	['@markup.raw']            = { bg = colors.bg_cursor },
-	['@markup.link.url']       = { fg = colors.cyan, underline = true },
+	['@markup.raw']            = { fg = colors.green },
+	['@markup.link.url']       = { fg = colors.blue, underline = true },
 
 	-- Error Handling
-	['@error']                 = { fg = colors.error_red, bold = true },
+	['@error']                 = { fg = colors.red, bold = true },
 
 	-----------------------------------------------------------------------------
 	-- Language-Specific Overrides
 	-----------------------------------------------------------------------------
 	-- Lua
-	['@variable.builtin.lua']  = { fg = colors.purple, bold = true }, -- _G, etc.
+	['@variable.builtin.lua']  = { fg = colors.red }, -- _G, etc.
 
 	-- Statusline specials
-	User1                      = { bg = '#6AC935', fg = 'black' },
-	User2                      = { bg = '#8F0000', fg = 'white' },
+	User1                      = { bg = colors.green, fg = colors.bg },
+	User2                      = { bg = colors.red, fg = '#FFFFFF' },
 }
 
 return scheme
